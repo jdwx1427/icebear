13e850aee094067a84fb5b2eab402bc9ec251da5
zhouqi <zhouqi1427@163.com>
2024-05-29 17:05:08 +0800

    add

--- app/publicnum/controller/pubMonitor.php
+++ app/publicnum/controller/pubMonitor.php
@@ -1,29 +1,28 @@
 <?php
 
-namespace app\wechat\controller;
+namespace app\publicnum\controller;
 
 use think\facade\Db;
 use think\facade\Log;
 use think\facade\Request;
 
-class WeChatMonitor
+class pubMonitor
 {
     public function click()
     {
         // 获取请求参数
-        $wechat_unionid = Request::get('wechat_unionid');
+        $trace_id = Request::get('trace_id');
+        $wechat_openid = Request::get('wechat_openid');
+        $adgroup_id = Request::get('adgroup_id');
+        $act_time = Request::get('act_time');
         $campaign_id = Request::get('campaign_id');
         $ad_id = Request::get('ad_id');
-        $addway = Request::get('addway');
+        $wechat_account_id = Request::get('wechat_account_id');
+        $position_id = Request::get('position_id');
         $advertiser_id = Request::get('advertiser_id');
+        $device_os_type = Request::get('device_os_type');
+        $shared_ad = Request::get('shared_ad');
         $dynamic_creative_id = Request::get('dynamic_creative_id');
-        $act_type = Request::get('act_type');
-        $act_time = Request::get('act_time');
-        $trace_id = Request::get('trace_id');
-        $adgroup_id = Request::get('adgroup_id');
-        $click_time = Request::get('click_time');
-        $qywx_corp_id = Request::get('qywx_corp_id');
-        $add_channel = Request::get('add_channel');
         $callback = Request::get('callback');
         
         if(!$trace_id){
@@ -32,41 +31,34 @@ class WeChatMonitor
         if(!$adgroup_id){
             return json(['ret'=>443,'msg'=>'adgroup_id不能为空']);
         }
-        if(!$click_time){
-            return json(['ret'=>443,'msg'=>'click_time不能为空']);
-        }
-        if(!$qywx_corp_id){
-            return json(['ret'=>443,'msg'=>'qywx_corp_id不能为空']);
-        }
-        if(!$add_channel){
-            return json(['ret'=>443,'msg'=>'add_channel不能为空']);
+        if(!$act_time){
+            return json(['ret'=>443,'msg'=>'act_time不能为空']);
         }
         if(!$callback){
             return json(['ret'=>443,'msg'=>'callback不能为空']);
         }
 
         $params = [
-            'wechat_unionid' => $wechat_unionid ?? '',
+            'trace_id' => $trace_id ?? '',
+            'wechat_openid' => $wechat_openid ?? '',
+            'adgroup_id' => $adgroup_id ?? '',
+            'act_time' => $act_time ?? '',
             'campaign_id' => $campaign_id ?? '',
             'ad_id' => $ad_id ?? '',
-            'addway' => $addway ?? '',
+            'wechat_account_id' => $wechat_account_id ?? '',
+            'position_id' => $position_id ?? '',
             'advertiser_id' => $advertiser_id ?? '',
+            'device_os_type' => $device_os_type ?? '',
+            'shared_ad' => $shared_ad ?? '',
             'dynamic_creative_id' => $dynamic_creative_id ?? '',
-            'act_type' => $act_type ?? '',
-            'act_time' => $act_time ?? '',
-            'trace_id' => $trace_id ?? '',
-            'adgroup_id' => $adgroup_id ?? '',
-            'click_time' => $click_time ?? '',
-            'qywx_corp_id' => $qywx_corp_id ?? '',
-            'add_channel' => $add_channel ?? '',
             'callback' => $callback ?? ''
         ];
 
         // 记录日志
-        Log::info('wechatMonitor ----- Ad Click', $params);
+        Log::info('pubMonitor ----- Ad Click', $params);
 
         // 保存到数据库
-        $res = Db::name('wechat')->insert($params);
+        $res = Db::name('publicnum')->insert($params);
         if(!$res){
             return json(['ret' => 0,'msg'=>$res]);
         }
